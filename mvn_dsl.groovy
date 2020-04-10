72edc8a9fed73fdcc9a869c57f37191ce6d891bc
Manish Kumar <manish.kumar@elucidata.io>
2020-04-10 13:48:12 +0530

    deb

--- mvn_dsl.groovy
+++ mvn_dsl.groovy
@@ -8,8 +8,8 @@ job('pbs_build_print') {
     }
 
     steps {
-        sh 'mvn clean'
-        sh 'mvn package'
-        sh 'java -jar target/pbs-0.0.1.jar'
+        shell('mvn clean')
+        shell('mvn package')
+        shell('java -jar target/pbs-0.0.1.jar')
     }
 }
