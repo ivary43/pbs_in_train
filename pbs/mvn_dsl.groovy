90fe69c430ca4733ac24defffeb14ea679931416
Manish Kumar <manish.kumar@elucidata.io>
2020-04-10 13:02:30 +0530

    debug

--- pbs/mvn_dsl.groovy
+++ pbs/mvn_dsl.groovy
@@ -2,13 +2,15 @@ job('pbs_build_print') {
     scm {
         github('ivary43/pbs_in_train', 'week_one')
     }
-    
+
     triggers {
        	scm('0 * * ? * *')
     }
 
     steps {
+    	shell("pwd")
         shell("cd pbs/")
+        shell("pwd")
         shell("mvn clean")
         shell("mvn package")
         shell("cd target/")
